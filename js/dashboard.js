3e0c7a30377fc70ea65091e45e0911bd289087da
Ahmedtaher820 <et8989784@gmail.com>
2022-05-13 20:51:25 +0200

    another commit

--- js/dashboard.js
+++ js/dashboard.js
@@ -68,7 +68,7 @@ accountSetting.forEach((e) => {
   });
 });
 // to get user date to display it
-let name = getElement("fname");
+let username = getElement("fname");
 let email = getElement("email");
 let companyName = getElement("company");
 let phone = getElement("phone");
@@ -80,9 +80,7 @@ let secondAddress = getElement("addresstwo");
 let city = getElement("city");
 let country = getElement("country");
 let zipCode = getElement("zip");
-function getElement(id) {
-  return document.getElementById(id);
-}
+
 window.addEventListener("load", getUserDate());
 async function getUserDate() {
   let userId = localStorage.getItem("user-market-id");
@@ -90,7 +88,8 @@ async function getUserDate() {
     .then((resolved) => resolved.json())
     .then((data) => {
       console.log(data);
-      name.value = data.user.name;
+      document.querySelector(".user-name").textContent = data.user.name;
+      username.value = data.user.name;
       email.value = data.user.email;
       companyName.value = data.user.companyName;
       phone.value = data.user.phone;
@@ -113,8 +112,74 @@ function productImgFun() {
   if (file.length > 0) {
     var fileReader = new FileReader();
     fileReader.onload = (event) => {
-      console.log(event.target.result);
+      var picture = event.target.result;
+      return picture;
     };
     fileReader.readAsDataURL(file[0]);
   }
+}
+
+// Create Product
+let productName = getElement("productname");
+let sellingPrice = getElement("sellingPrice");
+let buyPrice = getElement("buyPrice");
+let size = getElement("size");
+let color = getElement("color");
+let brand = getElement("brand");
+let productcountry = getElement("country");
+let productCity = getElement("productCity");
+let quantity = getElement("quantity");
+let productArray = [
+  productName,
+  sellingPrice,
+  buyPrice,
+  size,
+  color,
+  brand,
+  productcountry,
+  productCity,
+];
+// to check if inputs is empty or not
+productArray.forEach((e) => {
+  e.onkeyup = function () {
+    if (!e.value.length) {
+      e.style.border = "1px solid red";
+    } else {
+      e.style.border = "1px solid #ff9600";
+    }
+  };
+});
+// to send prosduct data to DB
+let err = document.querySelector(".error");
+async function createproduct() {
+  console.log(productcountry.value);
+
+  let userToken = localStorage.getItem("user-market-token");
+  await fetch(`https://emarket3.herokuapp.com/api/products/creatProducts`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      "x-auth-token": userToken,
+    },
+    body: JSON.stringify({
+      picture: productImgFun(),
+      name: productName.value,
+      sellingPrice: sellingPrice.value,
+      buyPrice: buyPrice.value,
+      size: size.value,
+      color: color.value,
+      brand: brand.value,
+      country: productcountry.value,
+      city: productCity.value,
+      quantity: quantity.value,
+    }),
+  })
+    .then((resolve) => resolve.json())
+    .then((data) => {
+      err.textContent = data.message;
+    });
+}
+
+function getElement(id) {
+  return document.getElementById(id);
 }
